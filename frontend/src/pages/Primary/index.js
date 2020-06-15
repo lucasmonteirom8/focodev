a8c7bea7ba42beb6de009b1cd38e608103780b52
lucasmonteirom8 <lucasmonteiro.m8@hotmail.com>
2020-06-15 15:48:55 +0000

    create: frontend architecture

---
+++ frontend/src/pages/Primary/index.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import './styles.css';
+
+export default function Index() {
+    return (
+        <div>
+            Hello World!
+        </div>
+    )
+}
