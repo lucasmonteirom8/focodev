b152c1c54ce995ccf0703cfb5843d73f4e0fa510
lucasmonteirom8 <lucasmonteiro.m8@hotmail.com>
2020-06-16 19:43:38 +0000

    create: route of delete user by id

--- backend/src/controllers/User.controller.ts
+++ backend/src/controllers/User.controller.ts
@@ -17,16 +17,32 @@ class User implements InterfaceControllers{
     public initRoutes(){
         this.router.get('/', this.index);
         this.router.post('/signup', this.signup);
+        this.router.delete('/delete', this.delete);
     }
 
     index = async (req: Request, res: Response) => {
         const allUser = await prisma.user.findMany();
         
-        console.log(JSON.stringify(allUser));
+        console.log(allUser);
     }
 
     delete = async (req: Request, res: Response) => {
+        const { id } = req.body;
+        
+        try{
+            const user = await prisma.user.delete({
+                where: {
+                    id
+                }
+            }).then(() => {
+                res.status(200).send({ message: "Deleted User Successfully", id });
+            }).catch((err) => {
+                res.status(400).send({ message: "Error to delete user, contact support"});
+            })
 
+        }catch(e){
+            res.status(400).send({message: 'Error at data transfer'});
+        }
     }
 
     update = async (req: Request, res: Response) => {
@@ -49,9 +65,9 @@ class User implements InterfaceControllers{
                     }
                 } 
             }).then(() => {
-                res.status(200).send({ message: "Created User Successfully", body: createdUser });
+                res.status(200).send({ message: "Created User Successfully", createdUser });
             }).catch((err) => {
-                res.status(400).send({ message: "Error creating user, contact support", error: err});
+                res.status(400).send({ message: "Error creating user, contact support"});
             })
 
         }catch(e){
